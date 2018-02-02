05a93bd9ed0d12dfacd3294d707836fcb2139989
samdd <sam@samdd.me>
2018-02-02 17:19:33 +0000

    Deployed 0303b87 with MkDocs version: 0.17.2

--- assets/js/crate.js
+++ assets/js/crate.js
@@ -1,3 +1,6 @@
+/**
+ * Discord button
+ */
 var crateTag = document.createElement('script')
 crateTag.src = 'https://crate.widgetbot.io/v2'
 crateTag.innerHTML = 'new Crate(' + JSON.stringify({
@@ -7,8 +10,24 @@ crateTag.innerHTML = 'new Crate(' + JSON.stringify({
   colors: {
     toggle: '#3F51B5'
   },
+  notifications: {
+    toasts: {
+      visibilityTime: 0
+    }
+  },
   style: 'material',
-  beta: false
+  beta: true
 }) + ')'
 
 document.head.appendChild(crateTag)
+
+/**
+ * Analytics
+ */
+var sc_project=11546194
+var sc_invisible=1
+var sc_security="b588ced0"
+var sc_https=1
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' async src='" + scJsHost + "statcounter.com/counter/counter_xhtml.js'></"+"script>")
