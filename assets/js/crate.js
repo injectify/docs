330a63423275935885b70e3604589c255e6d6b23
samdd <sam@samdd.me>
2018-03-03 12:09:02 +0000

    Deployed c7a4353 with MkDocs version: 0.17.2

--- assets/js/crate.js
+++ assets/js/crate.js
@@ -1,25 +1,28 @@
-/**
- * Discord button
- */
-var crateTag = document.createElement('script')
-crateTag.src = 'https://crate.widgetbot.io/v2'
-crateTag.innerHTML = 'new Crate(' + JSON.stringify({
-  server: '335836376031428618',
-  channel: '377173106940182529',
-  options: '0002',
-  colors: {
-    toggle: '#3F51B5'
-  },
-  notifications: {
-    toasts: {
-      visibilityTime: 0
-    }
-  },
-  style: 'material',
-  beta: true
-}) + ')'
+var params = new URLSearchParams(location.search)
+if (!params.has('embedded')) {
+  /**
+   * Discord button
+   */
+  var crateTag = document.createElement('script')
+  crateTag.src = 'https://crate.widgetbot.io/v2'
+  crateTag.innerHTML = 'new Crate(' + JSON.stringify({
+    server: '335836376031428618',
+    channel: '377173106940182529',
+    options: '0002',
+    colors: {
+      toggle: '#3F51B5'
+    },
+    notifications: {
+      toasts: {
+        visibilityTime: 0
+      }
+    },
+    style: 'material',
+    beta: true
+  }) + ')'
 
-document.head.appendChild(crateTag)
+  document.head.appendChild(crateTag)
+}
 
 /**
  * Analytics
